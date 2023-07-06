4425f4c0a53693c6893b4215a2491aa12ab6e933
zhaopeng.cl <zhaopeng.cl@bytedance.com>
2023-07-06 10:23:39 +0800

    feat: map Q to save file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- 保存本地变量
+local map = vim.api.nvim_set_keymap
+local opt = { noremap = true, silent = true }
+map("n", "Q", ":w<CR>", opt)
