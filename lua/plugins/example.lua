177f9a5b00fefe5d553541b2b72d58b9f607db65
zhaopeng.cl <zhaopeng.cl@bytedance.com>
2023-08-25 14:18:13 +0800

    Revert "fix: menorepo tsserver crash"
    This reverts commit 729ce1dc4e6365ad34c197be4b34c994a2be67c9.

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -121,10 +121,7 @@ return {
       setup = {
         -- example to setup with typescript.nvim
         tsserver = function(_, opts)
-          require("typescript").setup({
-            server = opts, 
-            root_dir = require('lspconfig.util').root_pattern('.git'),
-          })
+          require("typescript").setup({ server = opts })
           return true
         end,
         -- Specify * to use this function as a fallback for any server
