c81b9f5664285229bc220374229690135ba8cc1d
zhaopeng.cl <zhaopeng.cl@bytedance.com>
2023-10-15 23:18:03 +0800

    feat:  treesitter 支持使用快捷键选中 node

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -155,6 +155,15 @@ return {
         "vim",
         "yaml",
       },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-;>",
+        node_incremental = "<C-;>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      }
+    }
     },
   },
 
